23b5e380d4ac1f41826deadea20ef53688001095
loqman <loqmanalhakim.aripin@petronas.com>
2024-10-21 12:53:49 +0800

    added analytics

--- app/(dashboard)/teacher/analytics/page.tsx
+++ app/(dashboard)/teacher/analytics/page.tsx
@@ -1,8 +1,45 @@
-import React from 'react'
+import { getAnalytics } from "@/actions/get-analytics";
+import { createClient } from "@/utils/supabase/server"
+import { redirect } from "next/navigation";
+import { DataCard } from "./_components/data-card";
+import { Chart } from "./_components/chart";
 
-function Analytics() {
+async function Analytics() {
+  const supabase = createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    return redirect("/");
+  }
+
+
+  const {
+    data,
+    totalRevenue,
+    totalSales,
+  } = await getAnalytics(user.id);
+
+
   return (
-    <div>Analytics</div>
+    <div className="p-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mb-4">
+        <DataCard
+          label="Total revenue"
+          value={totalRevenue}
+          shouldFormat
+        />
+        <DataCard
+          label="Total sales"
+          value={totalSales}
+        />
+      </div>
+      <Chart
+        data={data}
+      />
+    </div>
   )
 }
 
